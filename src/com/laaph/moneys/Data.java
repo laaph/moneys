c28f7ae604ce26b50eedac50b83fecda048904ba
Stanza <stanza@techfocus.net>
2010-11-02 22:24:49 -0400

    Added basic data structure

---
+++ src/com/laaph/moneys/Data.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+import java.util.Date;
+
+class Data {
+    ArrayList<Date>      dates = new ArrayList<Date>();
+    ArrayList<String>   labels = new ArrayList<String>();
+    ArrayList<Integer>   items = new ArrayList<Integer>();
+    ArrayList<String> comments = new ArrayList<String>();
+}
