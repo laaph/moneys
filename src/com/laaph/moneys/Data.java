22fd0b14d2bac21505a8ade3f148aa266ca643b5
Stanza <stanza@techfocus.net>
2010-11-02 22:34:02 -0400

    Added add(...) method to Data.java

--- src/com/laaph/moneys/Data.java
+++ src/com/laaph/moneys/Data.java
@@ -7,4 +7,9 @@ class Data {
     ArrayList<String>   labels = new ArrayList<String>();
     ArrayList<Integer>   items = new ArrayList<Integer>();
     ArrayList<String> comments = new ArrayList<String>();
+
+    public void addEntry(Date d, String l, Integer i, String c) {
+	dates.add(d); labels.add(l); items.add(i); comments.add(c);
+    }
+
 }
